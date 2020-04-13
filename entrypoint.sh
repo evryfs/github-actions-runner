e7351376ebb9b7e9ce8b4fd6cbe233b1e1728cd3
David Karlsen <david@davidkarlsen.com>
2020-04-13 18:50:48 +0200

    output raw text

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-RUNNER_TOKEN=$(curl -H "Authorization: token ${GH_TOKEN}" -XPOST https://api.github.com/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token| jq .token)
+RUNNER_TOKEN=$(curl -H "Authorization: token ${GH_TOKEN}" -XPOST https://api.github.com/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token| jq -r .token)
 REPO_URL=https://github.com/${GH_ORG}/${GH_REPO}
 ./config.sh --unattended --replace --url ${REPO_URL} --token ${RUNNER_TOKEN}
 exec ./run.sh
