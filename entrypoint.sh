42940dc6d07109bbed4bc63e68d50ce48162164a
David Karlsen <david@davidkarlsen.com>
2020-04-13 18:44:19 +0200

    self-register the runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
-./config.sh --unattended --replace --url ${REPO_URL} --token ${GH_TOKEN}
+RUNNER_TOKEN=$(curl -H "Authorization: token ${GH_TOKEN}" -XPOST https://api.github.com/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token| jq .token)
+REPO_URL=https://github.com/${GH_ORG}/${GH_REPO}
+./config.sh --unattended --replace --url ${REPO_URL} --token ${RUNNER_TOKEN}
 exec ./run.sh
