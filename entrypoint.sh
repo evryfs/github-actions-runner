0b5d74685442faffde8aac70abab54df08d05057
David Karlsen <david@davidkarlsen.com>
2020-04-14 13:04:41 +0200

    install signal-handler

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,15 @@
 set -e
 readonly GH_API_ENDPOINT=https://api.github.com
 
+removeRunner() {
+  ./config.sh remove --unattended
+}
+
 RUNNER_TOKEN=${RUNNER_TOKEN:-$(curl -sL -H "Authorization: token ${GH_TOKEN}" -XPOST "${GH_API_ENDPOINT}/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token"| jq -r .token)}
 RUNNER_URL=${RUNNER_URL-$([[ -z "$GH_REPO" ]] && echo "https://github.com/${GH_ORG}/${GH_REPO}" || echo "https://github.com/${GH_ORG}" )}
 ./config.sh --unattended --replace --url "${RUNNER_URL}" --token "${RUNNER_TOKEN}"
+
+trap 'removeRunner' QUIT TERM
+
 RUNNER_ARGS="--once"
-exec "./run.sh ${RUNNER_ARGS}"
-./config.sh remove --unattended
+exec "./run.sh" "${RUNNER_ARGS}"
