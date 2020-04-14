b965486be2404ec051520b52cb01c09f77a3184f
David Karlsen <david@davidkarlsen.com>
2020-04-14 12:51:18 +0200

    by default run once

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ readonly GH_API_ENDPOINT=https://api.github.com
 RUNNER_TOKEN=${RUNNER_TOKEN:-$(curl -sL -H "Authorization: token ${GH_TOKEN}" -XPOST "${GH_API_ENDPOINT}/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token"| jq -r .token)}
 RUNNER_URL=${RUNNER_URL-$([[ -z "$GH_REPO" ]] && echo "https://github.com/${GH_ORG}/${GH_REPO}" || echo "https://github.com/${GH_ORG}" )}
 ./config.sh --unattended --replace --url "${RUNNER_URL}" --token "${RUNNER_TOKEN}"
-exec ./run.sh
+RUNNER_ARGS="--once"
+exec "./run.sh ${RUNNER_ARGS}"
+./config.sh remove --unattended
