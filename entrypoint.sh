87665d9206b7a0371477f2d2487fa986268087a2
David Karlsen <david@davidkarlsen.com>
2020-04-14 11:02:43 +0200

    allow escape-hatch for runner_url

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 readonly GH_API_ENDPOINT=https://api.github.com
 
 RUNNER_TOKEN=${RUNNER_TOKEN:-$(curl -sL -H "Authorization: token ${GH_TOKEN}" -XPOST "${GH_API_ENDPOINT}/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token"| jq -r .token)}
-RUNNER_URL=$([[ -z "$GH_REPO" ]] && echo "https://github.com/${GH_ORG}/${GH_REPO}" || echo "https://github.com/${GH_ORG}" )
+RUNNER_URL=${RUNNER_URL-$([[ -z "$GH_REPO" ]] && echo "https://github.com/${GH_ORG}/${GH_REPO}" || echo "https://github.com/${GH_ORG}" )}
 ./config.sh --unattended --replace --url "${RUNNER_URL}" --token "${RUNNER_TOKEN}"
 exec ./run.sh
