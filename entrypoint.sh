9e612bd169bef75b3cd843a47626c6c9b15d8ebc
David Karlsen <david@davidkarlsen.com>
2020-04-14 10:49:32 +0200

    support org wide runners

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
-
 readonly GH_API_ENDPOINT=https://api.github.com
-RUNNER_TOKEN=$(curl -sL -H "Authorization: token ${GH_TOKEN}" -XPOST ${GH_API_ENDPOINT}/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token| jq -r .token)
 
-REPO_URL=https://github.com/${GH_ORG}/${GH_REPO}
-./config.sh --unattended --replace --url ${REPO_URL} --token ${RUNNER_TOKEN}
+RUNNER_TOKEN=${RUNNER_TOKEN:-$(curl -sL -H "Authorization: token ${GH_TOKEN}" -XPOST "${GH_API_ENDPOINT}/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token"| jq -r .token)}
+RUNNER_URL=$([[ -z "$GH_REPO" ]] && echo "https://github.com/${GH_ORG}/${GH_REPO}" || echo "https://github.com/${GH_ORG}" )
+./config.sh --unattended --replace --url "${RUNNER_URL}" --token "${RUNNER_TOKEN}"
 exec ./run.sh
