b8f028578df3240d0ae2448fabad416dda950886
David Karlsen <david@davidkarlsen.com>
2020-11-01 12:37:59 +0100

    fix tagging script

--- tag.sh
+++ tag.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-RUNNER_VERSION=$(fgrep "ARG RUNNER_VERSION" Dockerfile |cut -d = -f 2)
-VIRTUAL_ENVIRONMENT_VERSION=$(fgrep "ARG VIRTUAL_ENVIRONMENT_VERSION" Dockerfile| cut -d = -f 2 | sed s/\\//-/)
+RUNNER_VERSION=$(grep "ARG RUNNER_VERSION" -F Dockerfile |cut -d = -f 2)
+VIRTUAL_ENVIRONMENT_VERSION=$(grep "ARG VIRTUAL_ENVIRONMENT_VERSION" -F Dockerfile| cut -d = -f 2 | sed s/\\//-/)
 TAG=${VIRTUAL_ENVIRONMENT_VERSION}-${RUNNER_VERSION}
+UBUNTU_VERSION=ubuntu-$(grep "ARG UBUNTU_VERSION" -F Dockerfile | cut -d = -f 2)
 
-git tag ${TAG}
+git tag "${TAG}"
+git tag -f "${UBUNTU_VERSION}"
