ea264d975531eac62c274cf424a33fd3886d8d5c
David J. M. Karlsen <david@davidkarlsen.com>
2020-12-14 18:57:55 +0100

    no need to tag ubuntu20 - same as branch

--- tag.sh
+++ tag.sh
@@ -3,7 +3,5 @@
 RUNNER_VERSION=$(grep "ARG RUNNER_VERSION" -F Dockerfile |cut -d = -f 2)
 VIRTUAL_ENVIRONMENT_VERSION=$(grep "ARG VIRTUAL_ENVIRONMENT_VERSION" -F Dockerfile| cut -d = -f 2 | sed s/\\//-/)
 TAG=${VIRTUAL_ENVIRONMENT_VERSION}-${RUNNER_VERSION}
-UBUNTU_VERSION=ubuntu-$(grep "ARG UBUNTU_VERSION" -F Dockerfile | cut -d = -f 2)
 
 git tag "${TAG}"
-git tag -f "${UBUNTU_VERSION}"
