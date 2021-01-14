1012b86dedf8b641376731c6dbb6869ff4179fa0
David J. M. Karlsen <david@davidkarlsen.com>
2021-01-14 22:16:00 +0100

    Cleanups for suiting operator v0.8.0 (#39)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -e
-readonly GH_API_ENDPOINT=https://api.github.com
+
+if [ -z "$RUNNER_TOKEN" ]
+then
+  echo "Must define RUNNER_TOKEN variable"
+  exit 255
+fi
 
 if [ -z "$GH_REPO" ]
 then
-  # org-level runners: https://github.com/actions/runner/issues/245
-  readonly TOKEN_URL=${GH_API_ENDPOINT}/orgs/${GH_ORG}/actions/runners/registration-token
   readonly RUNNER_URL=https://github.com/${GH_ORG}
 else
-  # per repo runner
-  readonly TOKEN_URL=${GH_API_ENDPOINT}/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token
   readonly RUNNER_URL="https://github.com/${GH_ORG}/${GH_REPO}"
 fi
 
-RUNNER_TOKEN=${RUNNER_TOKEN:-$(curl -sL -H "Authorization: token ${GH_TOKEN}" -XPOST "${TOKEN_URL}" | jq -r .token)}
 ./config.sh --unattended --replace --url "${RUNNER_URL}" --token "${RUNNER_TOKEN}"
 exec "./run.sh" "${RUNNER_ARGS}"
