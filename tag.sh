0010ab614e7a382cfed1d4a16c1e35959e26c19a
Otterian <eth0linak@gmail.com>
2021-09-08 14:42:26 +0200

    Remove runner from tag.sh, now we only tag with virtualenv.

--- tag.sh
+++ tag.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-RUNNER_VERSION=$(grep "ARG RUNNER_VERSION" -F Dockerfile |cut -d = -f 2)
 VIRTUAL_ENVIRONMENT_VERSION=$(grep "ARG VIRTUAL_ENVIRONMENT_VERSION" -F Dockerfile| cut -d = -f 2 | sed s/\\//-/)
-TAG=${VIRTUAL_ENVIRONMENT_VERSION}-${RUNNER_VERSION}
+TAG=${VIRTUAL_ENVIRONMENT_VERSION}
 
 git tag "${TAG}"
