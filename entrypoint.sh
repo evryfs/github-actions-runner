8bb89e3b6e0919d3255b112e5830691225c395f8
Denis Baryshev <dennybaa@gmail.com>
2021-11-23 12:18:48 +0300

    fix runner re-registration issue (#96)

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,5 +18,8 @@ fi
 
 sudo install-runner
 
-./config.sh --unattended --replace --url "${RUNNER_URL}" --token "${RUNNER_TOKEN}"
+# Reconfigure from the clean state in case of runner failures/restarts
+./config.sh remove --token "${RUNNER_TOKEN}"
+./config.sh --unattended --url "${RUNNER_URL}" --token "${RUNNER_TOKEN}"
+
 exec "./run.sh" "${RUNNER_ARGS}"
